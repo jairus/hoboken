0b0a1d1befd2d1e8a907c047a94adde39a0a6e84
Rhev Gutierrez <rhev@nmgresources.ph>
2012-10-29 13:55:21 +0800

    initial system interface

---
+++ Mommies 247 System Interface/mommies247/form.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mommies 247 | Site List</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+	<div id="mom247-container">
+    	<table width="100%" cellspacing="0" cellpadding="0">
+        	<tbody>
+            	<tr>
+                	<td id="header">
+                    	<table width="100%" cellspacing="0" cellpadding="0">
+                        	<tbody>
+                            	<tr>
+                                	<td><img src="images/header-logo.png"/></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr> <!--end of header -->
+                <tr>
+                	<td id="menu">
+                			    
+                    </td>
+                </tr> <!-- end of menu -->
+                
+                <tr>
+                	<td id="content">
+                        <form method="post" id="site_form">
+                        <table width="100%" cellpadding="10px">
+                        <tbody>
+                          <tr>
+                          	<td class="bold font18">Create New Site</td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                          	<td>
+                                <table width="100%" cellpadding="2">
+                                	<tr>
+                            <td>Subdomain</td>                            
+                            <td><input type="text" name="subdomain_name" size="40">.mommies247.com</td>
+                          </tr>
+                          <tr>
+                            <td>Site Title</td>
+                            <td><input type="text" name="site_title" size="40"></td>
+                          </tr>
+                          <tr>
+                            <td>Site Admin Username:</td>
+                            <td>
+                            <input type="text" name="admin_username" size="35"/>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Site Admin Password:</td>
+                            <td>
+                            <input type="password" name="admin_password" size="35"/>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Admin Email Address: </td>
+                            <td><input type="text" name="email_addr" size="35"></td>
+                          </tr>
+                                </table>
+                            </td>
+                            <td>
+                                <table width="100%">
+                                <tr>
+                                  <td>Site Logo:</td>
+                                  <td><input type="file" name="upload_logo" /></td>
+                          		</tr>
+                                </table>
+                            </td>
+                          </tr>
+                          </tbody>
+                        </table>
+                        <p><input type="submit" value="Create" name="sub1"></p>
+                        </form>
+                        
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="footer">
+        	<img src="images/footer-content-image.png" />
+        </div>
+    </div>
+</body>
+</html>
